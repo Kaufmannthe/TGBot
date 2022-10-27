ffae72cdb1d85dbb3151334b9ccd1b1e0237e98d
Artyom Bohan <kaufmannthe@gmail.com>
2022-10-27 04:28:55 +0300

    initial commit

--- src/main/java/com/bot/tgbot/service/TelegramBot.java
+++ src/main/java/com/bot/tgbot/service/TelegramBot.java
@@ -1,2 +1,68 @@
-package com.bot.tgbot.service;public class TelegramBot {
+package com.bot.tgbot.service;
+
+import com.bot.tgbot.config.BotConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+@Component
+public class TelegramBot extends TelegramLongPollingBot {
+
+    private final BotConfig config;
+
+    @Autowired
+    public TelegramBot(BotConfig config) {
+        this.config = config;
+    }
+
+
+    @Override
+    public String getBotUsername() {
+        return config.getBotName();
+    }
+
+    @Override
+    public String getBotToken() {
+        return config.getToken();
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+
+        if (update.hasMessage() && update.getMessage().hasText()){
+            String message = update.getMessage().getText();
+            long chatId = update.getMessage().getChatId();
+            switch (message){
+                case "/start" -> {
+                    try {
+                        startCommand(chatId, update.getMessage().getChat().getFirstName());
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                default -> {
+                    try {
+                        sendMessage(chatId, "Sorry, command was not recognized");
+                    } catch (TelegramApiException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+            }
+        }
+
+    }
+    private void startCommand(long chatId, String name) throws TelegramApiException {
+        String answer = "Hi, " + name +", nice to meet you";
+        sendMessage(chatId, answer);
+    }
+    private void sendMessage(long chatId, String text) throws TelegramApiException {
+        SendMessage message = new SendMessage();
+        message.setChatId(chatId);
+        message.setText(text);
+        execute(message);
+    }
 }
